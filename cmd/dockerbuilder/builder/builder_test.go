4e138c48c01736efb3eb9de525466a70bb6f45a8
tangfeixiong <tangfx128@gmail.com>
2016-05-01 10:52:51 +0000

    demo with openshit docker builder service end

--- cmd/dockerbuilder/builder/builder_test.go
+++ cmd/dockerbuilder/builder/builder_test.go
@@ -1,145 +1,144 @@
-
 package builder
 
 import (
-    "bytes"
-    _ "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "testing"
+	"bytes"
+	_ "encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
 
 	"github.com/ghodss/yaml"
-    "github.com/helm/helm/codec"    
-    oapi "github.com/openshift/origin/pkg/build/api"
-    
+	"github.com/helm/helm/codec"
+	oapi "github.com/openshift/origin/pkg/build/api"
 )
 
 func TestExportBuild(t *testing.T) {
-    
-    o := oapi.Build { }
-    o.TypeMeta.Kind = "Build"
-    o.TypeMeta.APIVersion = "v1"
-    
-    o.ObjectMeta.Name = "build101"
-    o.ObjectMeta.Labels = make(map[string]string)
-    o.ObjectMeta.Labels["ci"] = "build101"
-    o.ObjectMeta.Labels["name"] = "build101"
-    
-    fillBuildSpec(&o.Spec)
-    
-    b := new(bytes.Buffer)
-    err := codec.JSON.Encode(b).One(o)
-    if err != nil {
-        t.Errorf("Could not encode JSON object: %s", err)
-    }
-    fmt.Println(b.String())
-    
-    y, err := yaml.Marshal(o)
-    if err != nil {
-        t.Errorf("Could not encode YAML object: %s", err)
-    }
-    fmt.Println(string(y))
-    
-    /*
-    o, err := codec.JSON.Decode(b.Bytes()).One()
-    if err != nil {
-        t.Errorf("Could not build YAML decoder: %s", err)
-    }
-    yaml, err := o.YAML()
-    if err != nil {
-        t.Errorf("Could not decode into YAML: %s", err)
-    }
-    fmt.Println(string(yaml))
-    */
+
+	o := oapi.Build{}
+	o.TypeMeta.Kind = "Build"
+	o.TypeMeta.APIVersion = "v1"
+
+	o.ObjectMeta.Name = "build101"
+	o.ObjectMeta.Labels = make(map[string]string)
+	o.ObjectMeta.Labels["ci"] = "build101"
+	o.ObjectMeta.Labels["name"] = "build101"
+	o.ObjectMeta.Labels["namespace"] = "tangfeixiong"
+
+	fillBuildSpec(&o.Spec)
+
+	b := new(bytes.Buffer)
+	err := codec.JSON.Encode(b).One(o)
+	if err != nil {
+		t.Errorf("Could not encode JSON object: %s", err)
+	}
+	fmt.Println(b.String())
+
+	y, err := yaml.Marshal(o)
+	if err != nil {
+		t.Errorf("Could not encode YAML object: %s", err)
+	}
+	fmt.Println(string(y))
+
+	/*
+	   o, err := codec.JSON.Decode(b.Bytes()).One()
+	   if err != nil {
+	       t.Errorf("Could not build YAML decoder: %s", err)
+	   }
+	   yaml, err := o.YAML()
+	   if err != nil {
+	       t.Errorf("Could not decode into YAML: %s", err)
+	   }
+	   fmt.Println(string(yaml))
+	*/
 }
 
 func TestImportBuild(t *testing.T) {
-    wd, err := os.Getwd()
-    if err != nil {
-        t.Errorf("Could not get PWD: %s", err)
-    }
-    fmt.Println(wd)
-    b, err := ioutil.ReadFile(wd + "/../../../examples/build101.yaml")
-    if err != nil {
-        t.Errorf("Could not get YAML content: %s", err)
-    }
-    fmt.Println(string(b))
-    o := new(oapi.Build)
-    err = yaml.Unmarshal(b, o)
-    if err != nil {
-        t.Errorf("Could not decode into Config Object: %s", err)
-    }
-    fmt.Printf("%+v", o)
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Errorf("Could not get PWD: %s", err)
+	}
+	fmt.Println(wd)
+	b, err := ioutil.ReadFile(wd + "/../../../examples/build101.yaml")
+	if err != nil {
+		t.Errorf("Could not get YAML content: %s", err)
+	}
+	fmt.Println(string(b))
+	o := new(oapi.Build)
+	err = yaml.Unmarshal(b, o)
+	if err != nil {
+		t.Errorf("Could not decode into Config Object: %s", err)
+	}
+	fmt.Printf("%+v", o)
 }
 
 func fillBuildSpec(spec *oapi.BuildSpec) {
-  if spec == nil { panic("unexpected") }
-  fillBuildSource(&spec.Source)
-  fillBuildOutput(&spec.Output)
-  fillBuildPostCommitSpec(&spec.PostCommit)
+	if spec == nil {
+		panic("unexpected")
+	}
+	fillBuildSource(&spec.Source)
+	fillBuildOutput(&spec.Output)
+	fillBuildPostCommitSpec(&spec.PostCommit)
 }
 
 func fillBuildSource(src *oapi.BuildSource) {
-  s := `From alpine`
-  src.Dockerfile = &s
+	s := `From alpine`
+	src.Dockerfile = &s
 }
 
 func createGitBuildSource() *oapi.GitBuildSource {
-    s := &oapi.GitBuildSource {
-        URI: "http://github.com/stackdocker/container-ops.git",
-        Ref: "gogs",
-    }
-    return s
+	s := &oapi.GitBuildSource{
+		URI: "http://github.com/stackdocker/container-ops.git",
+		Ref: "gogs",
+	}
+	return s
 }
 
 func fillBuildOutput(out *oapi.BuildOutput) {
-  
+
 }
 
 func fillBuildPostCommitSpec(commit *oapi.BuildPostCommitSpec) {
-  
+
 }
 
 func TestExportBuildConfig(t *testing.T) {
-    
-    o := oapi.BuildConfig { }
-    o.TypeMeta.APIVersion = "v1"
-    o.TypeMeta.Kind = "BuildConfig"
-    
-    o.ObjectMeta.Name = "buildconfig101"
-    o.ObjectMeta.Labels = make(map[string]string)
-    o.ObjectMeta.Labels["ci"] = "buildconfig101"
-    o.ObjectMeta.Labels["name"] = "buildconfig101"
-    
-    fillBuildSpec(&o.Spec.BuildSpec)
-    
-    b := new(bytes.Buffer)
-    err := codec.JSON.Encode(b).One(o)
-    if err != nil {
-        t.Errorf("Could not encode JSON object: %s", err)
-    }
-    fmt.Println(b.String())
-    
-    /*
-    o, err := codec.JSON.Decode(b.Bytes()).One()
-    if err != nil {
-        t.Errorf("Could not build YAML decoder: %s", err)
-    }
-    yaml, err := o.YAML()
-    if err != nil {
-        t.Errorf("Could not decode into YAML: %s", err)
-    }
-    fmt.Println(string(yaml))
-    */
-    y, err := yaml.Marshal(o)
-    if err != nil {
-        t.Errorf("Could not encode YAML object: %s", err)
-    }
-    fmt.Println(string(y))
+
+	o := oapi.BuildConfig{}
+	o.TypeMeta.APIVersion = "v1"
+	o.TypeMeta.Kind = "BuildConfig"
+
+	o.ObjectMeta.Name = "buildconfig101"
+	o.ObjectMeta.Labels = make(map[string]string)
+	o.ObjectMeta.Labels["ci"] = "buildconfig101"
+	o.ObjectMeta.Labels["name"] = "buildconfig101"
+
+	fillBuildSpec(&o.Spec.BuildSpec)
+
+	b := new(bytes.Buffer)
+	err := codec.JSON.Encode(b).One(o)
+	if err != nil {
+		t.Errorf("Could not encode JSON object: %s", err)
+	}
+	fmt.Println(b.String())
+
+	/*
+	   o, err := codec.JSON.Decode(b.Bytes()).One()
+	   if err != nil {
+	       t.Errorf("Could not build YAML decoder: %s", err)
+	   }
+	   yaml, err := o.YAML()
+	   if err != nil {
+	       t.Errorf("Could not decode into YAML: %s", err)
+	   }
+	   fmt.Println(string(yaml))
+	*/
+	y, err := yaml.Marshal(o)
+	if err != nil {
+		t.Errorf("Could not encode YAML object: %s", err)
+	}
+	fmt.Println(string(y))
 }
-
-
 
 var buildStr = `apiVersion: v1
 kind: Build
